bd2c1a4fc938612fbd84bf0dcdd723336d78b149
gquispec21 <gino_21quispe@outlook.com>
2024-05-30 18:31:41 -0500

    Repaso CSS, JS y creando proyecto Django

---
+++ MODULO_6/javascript/scripts.js
@@ -0,0 +1,24 @@
+console.log("hello world")
+
+for(let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+var nombre = "Gino"
+
+function saludar(name) {
+    console.log("Mi nombre es " + name)
+}
+saludar(nombre)
+
+let array = ["David", "Gino", "Jose"]
+console.log(array[1])
+
+let objeto = {
+    nombre: "Gino Quispe Calixto",
+    edad: 23,
+    correo: "quispecalixtogino@gmail.com"
+}
+
+console.log(objeto)
+console.log(typeof objeto)
